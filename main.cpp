85674c1ac0e87e313e79ac22a9444b76e436fbbb
pi <pi@modular-t.org>
2013-07-08 16:53:50 +0200

    fixed iss#1

--- main.cpp
+++ main.cpp
@@ -3,25 +3,8 @@
 
 int threads;
 
-class thread
-{
-	public:
-		int pub;
-		thread() { pub=0; no = threads++; }
-		void countplus()
-		{
-			pub++; 
-			printf("Thread %d  count %d\n", no, pub);
-		}
-	protected:
-		int no;
-};
-
 int main( int argc, char *argv[])
 {
 
-	thread nummer_1, nummer_2;
-	nummer_1.countplus();
-	nummer_2.countplus();
 	return 0;
 }
